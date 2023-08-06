1af870b83ba337a2b7b99a9760c2d8b7492eb80b
PavlovMaxim <makc_im7@bk.ru>
2023-08-06 23:02:30 +0300

    update serializer ingredient1

--- backend/recepts/serializers.py
+++ backend/recepts/serializers.py
@@ -144,11 +144,11 @@ class ReceptSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 'Не выбрано ни одного ингридиента'
             )
-        for ingredient in ingredients:
-            if ingredient in ingredients:
-                raise serializers.ValidationError(
-                    'Вы добавляете одно и то же'
-                )
+        # for ingredient in ingredients:
+        #     if ingredient in ingredients:
+        #         raise serializers.ValidationError(
+        #             'Вы добавляете одно и то же'
+        #         )
         all_ingredients = [ingredient['id'] for ingredient in ingredients]
         if len(all_ingredients) != len(set(all_ingredients)):
             raise serializers.ValidationError(

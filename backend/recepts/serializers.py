2fdc903135456cfe90ace65343528028232d6b15
PavlovMaxim <makc_im7@bk.ru>
2023-08-07 15:54:51 +0300

    serializers ingredients validate3

--- backend/recepts/serializers.py
+++ backend/recepts/serializers.py
@@ -144,11 +144,22 @@ class ReceptSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 'Не выбрано ни одного ингридиента'
             )
-        all_ingredients = [ingredient['id'] for ingredient in ingredients]
-        if len(all_ingredients) != len(set(all_ingredients)):
-            raise serializers.ValidationError(
-                'Ингридиенты не уникальны!'
+        # all_ingredients = [ingredient['id'] for ingredient in ingredients]
+        # if len(all_ingredients) != len(set(all_ingredients)):
+        #     raise serializers.ValidationError(
+        #         'Ингридиенты не уникальны!'
+        #     )
+        all_ingredients = ingredients["ingredients"]
+        ingredients_list = []
+        for ingredient in all_ingredients:
+            one_ingredient = get_object_or_404(
+                Ingredients, id=ingredient['id']
             )
+            if one_ingredient in ingredients_list:
+                raise serializers.ValidationError(
+                    'Такой ингридиент уже присутствует'
+                )
+            ingredients_list.append(one_ingredient)
         return ingredients
 
     def validate(self, attrs):

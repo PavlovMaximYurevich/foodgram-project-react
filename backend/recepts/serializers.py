f3932869bdb039bb5405293fbf6466a9770e93e2
PavlovMaxim <makc_im7@bk.ru>
2023-08-07 15:44:50 +0300

    serializers ingredients validate1

--- backend/recepts/serializers.py
+++ backend/recepts/serializers.py
@@ -144,15 +144,12 @@ class ReceptSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 'Не выбрано ни одного ингридиента'
             )
-        # all_ingredients = [ingredient['id'] for ingredient in ingredients]
-        # if len(all_ingredients) != len(set(all_ingredients)):
-        #     raise serializers.ValidationError(
-        #         'Ингридиенты не уникальны!'
-        #     )
-        return ingredients
-
-    def validate(self, attrs):
-        all_ingredients = attrs["ingredients"]
+        all_ingredients = [ingredient['id'] for ingredient in ingredients]
+        if len(all_ingredients) != len(set(all_ingredients)):
+            raise serializers.ValidationError(
+                'Ингридиенты не уникальны!'
+            )
+        all_ingredients = ingredients["ingredients"]
         ingredients_list = []
         for ingredient in all_ingredients:
             one_ingredient = get_object_or_404(
@@ -163,7 +160,21 @@ class ReceptSerializer(serializers.ModelSerializer):
                     'Такой ингридиент уже присутствует'
                 )
             ingredients_list.append(one_ingredient)
-        return attrs
+        return ingredients
+
+    # def validate(self, attrs):
+    #     all_ingredients = attrs["ingredients"]
+    #     ingredients_list = []
+    #     for ingredient in all_ingredients:
+    #         one_ingredient = get_object_or_404(
+    #             Ingredients, id=ingredient['id']
+    #         )
+    #         if one_ingredient in ingredients_list:
+    #             raise serializers.ValidationError(
+    #                 'Такой ингридиент уже присутствует'
+    #             )
+    #         ingredients_list.append(one_ingredient)
+    #     return attrs
 
     def create(self, validated_data):
         ingredients = validated_data.pop('ingredients')

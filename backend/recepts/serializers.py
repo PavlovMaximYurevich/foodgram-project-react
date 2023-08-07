15f977a661a46b3ced1828ba1af9d702f000b992
PavlovMaxim <makc_im7@bk.ru>
2023-08-07 15:49:30 +0300

    serializers ingredients validate2

--- backend/recepts/serializers.py
+++ backend/recepts/serializers.py
@@ -149,7 +149,10 @@ class ReceptSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 'Ингридиенты не уникальны!'
             )
-        all_ingredients = ingredients["ingredients"]
+        return ingredients
+
+    def validate(self, attrs):
+        all_ingredients = attrs["ingredients"]
         ingredients_list = []
         for ingredient in all_ingredients:
             one_ingredient = get_object_or_404(
@@ -160,21 +163,7 @@ class ReceptSerializer(serializers.ModelSerializer):
                     'Такой ингридиент уже присутствует'
                 )
             ingredients_list.append(one_ingredient)
-        return ingredients
-
-    # def validate(self, attrs):
-    #     all_ingredients = attrs["ingredients"]
-    #     ingredients_list = []
-    #     for ingredient in all_ingredients:
-    #         one_ingredient = get_object_or_404(
-    #             Ingredients, id=ingredient['id']
-    #         )
-    #         if one_ingredient in ingredients_list:
-    #             raise serializers.ValidationError(
-    #                 'Такой ингридиент уже присутствует'
-    #             )
-    #         ingredients_list.append(one_ingredient)
-    #     return attrs
+        return attrs
 
     def create(self, validated_data):
         ingredients = validated_data.pop('ingredients')

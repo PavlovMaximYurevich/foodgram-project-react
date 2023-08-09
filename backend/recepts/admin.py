86e58d5e65974b08fe3dbf33b218828a574eb7cd
PavlovMaxim <makc_im7@bk.ru>
2023-08-09 11:39:00 +0300

    has delete permissions

--- backend/recepts/admin.py
+++ backend/recepts/admin.py
@@ -15,16 +15,16 @@ class IngredientsInReceptAdmin(admin.TabularInline):
     min_num = 1
     # can_delete = False
 
-    # def has_delete_permission(self, request, obj=None):
-    #     if not self.model.ingredient:
-    #         return False
-    #     return True
-
-    def clean(self):
+    def has_delete_permission(self, request, obj=None):
         if not self.model.ingredient:
-            raise ValidationError(
-                "Рецепт не может иметь 0 ингредиетов"
-            )
+            return False
+        return True
+
+    # def clean(self):
+    #     if not self.model.ingredient:
+    #         raise ValidationError(
+    #             "Рецепт не может иметь 0 ингредиетов"
+    #         )
 
 
 class ReceptAdmin(admin.ModelAdmin):

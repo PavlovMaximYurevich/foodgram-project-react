8045a4f897374f358d50b0be2ac65067eaf2cd6c
PavlovMaxim <makc_im7@bk.ru>
2023-08-09 11:50:49 +0300

    self.model.amount

--- backend/recepts/admin.py
+++ backend/recepts/admin.py
@@ -13,18 +13,18 @@ from recepts.models import (Favourites,
 class IngredientsInReceptAdmin(admin.TabularInline):
     model = IngridientAmount
     min_num = 1
-    can_delete = False
+    # can_delete = False
 
     # def has_delete_permission(self, request, obj=None):
     #     if not self.model.ingredient:
     #         return False
     #     return True
 
-    # def clean(self):
-    #     if not self.model.ingredient:
-    #         raise ValidationError(
-    #             "Рецепт не может иметь 0 ингредиетов"
-    #         )
+    def clean(self):
+        if not self.model.ingredient and not self.model.amount:
+            raise ValidationError(
+                "Рецепт не может иметь 0 ингредиетов"
+            )
 
 
 class ReceptAdmin(admin.ModelAdmin):
